12dc309306101ed57e2d455d7ba31cfa34e4c133
dmackdev <dmackdev@gmail.com>
2023-09-16 08:19:36 +0100

    Added reset_expanded test.

--- tests/json_tree_test.rs
+++ tests/json_tree_test.rs
@@ -1,3 +1,4 @@
+use egui::{CentralPanel, Context, FontDefinitions, Style};
 use egui_json_tree::{DefaultExpand, JsonTree};
 use serde_json::json;
 
@@ -120,4 +121,85 @@ fn json_tree_default_expand_search() {
         "/foo/buzz",
     ];
     assert_eq!(expected_pointers, rendered_pointers);
+}
+
+#[test]
+fn json_tree_reset_expanded() {
+    let obj = json!({
+      "baz": {
+        "qux": 1
+      },
+      "buzz": [
+        1,
+      ]
+    });
+
+    // Reusing the same Context so the memory persists between multiple frames.
+    let ctx = Context::default();
+    ctx.set_fonts(FontDefinitions::empty());
+    ctx.set_style(Style {
+        animation_time: 0.0,
+        ..Default::default()
+    });
+
+    let expected_pointers_all = vec![
+        "/baz", "/baz/qux", "/baz/qux", "/buzz", "/buzz/0", "/buzz/0",
+    ];
+
+    let id = "id";
+
+    // First, render and expand everything.
+    // We expect everything to be expanded as this is the first render.
+    let _ = ctx.run(Default::default(), |ctx| {
+        let mut rendered_pointers = vec![];
+
+        CentralPanel::default().show(ctx, |ui| {
+            JsonTree::new(id, &obj)
+                .response_callback(|_, pointer| {
+                    rendered_pointers.push(pointer.to_string());
+                })
+                .default_expand(DefaultExpand::All)
+                .show(ui);
+        });
+
+        assert_eq!(expected_pointers_all, rendered_pointers);
+    });
+
+    // Next we render the same tree but change the `default_expand` setting.
+    // Because we already rendered the tree with everything expanded,
+    // we expect everything to be expanded still.
+    // Note that we call `reset_expanded` after rendering the tree.
+    let _ = ctx.run(Default::default(), |ctx| {
+        let mut rendered_pointers = vec![];
+
+        CentralPanel::default().show(ctx, |ui| {
+            JsonTree::new(id, &obj)
+                .response_callback(|_, pointer| {
+                    rendered_pointers.push(pointer.to_string());
+                })
+                .default_expand(DefaultExpand::None)
+                .show(ui)
+                .reset_expanded(ui);
+        });
+
+        assert_eq!(expected_pointers_all, rendered_pointers);
+    });
+
+    // Now we render again with the same `default_expand` setting as the last render.
+    // Because we called `reset_expanded` in the last frame, we now expect this setting to be respected,
+    // and now nothing should be expanded.
+    let _ = ctx.run(Default::default(), |ctx| {
+        let mut rendered_pointers = vec![];
+
+        CentralPanel::default().show(ctx, |ui| {
+            JsonTree::new(id, &obj)
+                .response_callback(|_, pointer| {
+                    rendered_pointers.push(pointer.to_string());
+                })
+                .default_expand(DefaultExpand::None)
+                .show(ui);
+        });
+
+        assert_eq!(vec!["", "", "", ""], rendered_pointers);
+    });
 }

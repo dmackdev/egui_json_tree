8606ec95c3355cd0025cb3abdcfb4c80ae8cc346
Nicolas <bircni@icloud.com>
2025-02-07 22:36:52 +0100

    fixes

--- examples/demo/src/apps/copy_to_clipboard.rs
+++ examples/demo/src/apps/copy_to_clipboard.rs
@@ -32,17 +32,15 @@ impl Show for CopyToClipboardExample {
                     .context_menu(|ui| {
                         let pointer = context.pointer().to_json_pointer_string();
                         if !pointer.is_empty() && ui.button("Copy path").clicked() {
-                            ui.output_mut(|_| {
-                                println!("{}", pointer);
-                                ui.ctx().copy_text(pointer);
-                            });
+                            println!("{}", pointer);
+                            ui.ctx().copy_text(pointer);
                             ui.close_menu();
                         }
 
                         if ui.button("Copy contents").clicked() {
                             if let Ok(pretty_str) = serde_json::to_string_pretty(context.value()) {
                                 println!("{}", pretty_str);
-                                ui.output_mut(|_| ui.ctx().copy_text(pretty_str));
+                                ui.ctx().copy_text(pretty_str);
                             }
                             ui.close_menu();
                         }
